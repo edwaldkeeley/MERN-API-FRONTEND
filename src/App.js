90aa06636df7aca5ea3e0c95661a5fb91657e5c0
edwaldkeeley <edwaldpk@gmail.com>
2023-04-19 16:23:30 +0700

    Update 10: Complete

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import NotesList from './features/notes/NotesList'
 import UsersList from './features/users/UsersList'
 import EditUser from './features/users/EditUser'
 import NewUserForm from './features/users/NewUserForm'
-import NewNote from './features/notes/NewNote'
 import EditNote from './features/notes/EditNote'
+import NewNote from './features/notes/NewNote'
 import Prefetch from './features/auth/Prefetch'
-// import NewNoteForm from './features/notes/NewNoteForm';
+import PersistLogin from './features/auth/PersistLogin'
 
 function App() {
   return (
@@ -20,23 +20,25 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=":id" element={<EditUser />} />
-              <Route path="new" element={<NewUserForm />} />
-            </Route>
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
 
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path=":id" element={<EditNote />} />
-              <Route path="new" element={<NewNote />} />
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
             </Route>
+            {/* End Dash */}
           </Route>
-          {/* end dash */}
         </Route>
       </Route>
     </Routes>
@@ -44,3 +46,4 @@ function App() {
 }
 
 export default App
+  
